b14f9746f0758e565e90d0608566771454e81d8b
ptmertka <71412047+ptmertka@users.noreply.github.com>
2023-04-10 20:20:25 -0500

    Buttons

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,53 @@ import './App.css';
 import { WaterFlowChart } from "./components/waterFlowChart";
 import {TemperatureChart} from "./components/TemperatureChart";
 import { WaterLevelChart } from './components/WaterLevelChart';
+import styled from 'styled-components'
+
+
+
+function heater() {
+
+	// const updates = {};
+	// updates['tempOn'] = true;
+
+	// update(ref(database), updates);
+	// console.log("Turned On Heater");
+  alert('You clicked refill')
+}
+function refill() {
+
+  // const updates = {};
+	// updates['fillTank'] = true;
+
+	// update(ref(database), updates);
+	// console.log("Refilled Tank");
+
+  alert('You clicked refill')
+}
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  font-size: 20px;
+  padding: 10px 60px;
+  border-radius: 5px;
+  margin: 10px 0px;
+  cursor: pointer;
+`;
+
+firebaseConfig = {
+  apiKey: "",
+  authDomain: "iotfinalproject-5a9e4.firebaseapp.com",
+  projectId: "iotfinalproject-5a9e4",
+  storageBucket: "iotfinalproject-5a9e4.appspot.com",
+  messagingSenderId: "340940497636",
+  appId: "1:340940497636:web:f361857a0309a1710ac2f1",
+  measurementId: "G-0FQ0WXNWBM"
+}
+
+firebase.initializeApp(firebaseConfig);
+
+const database = getDatabase();
 
 function App() {
   return (
@@ -26,8 +73,10 @@ function App() {
         <div id="chart1"><WaterFlowChart /></div>
         <div id="chart2"><WaterLevelChart /></div>
         <div id="chart3"><TemperatureChart /></div>
+        <div id="chart4"> <Button onClick={heater}>Turn On Heater</Button>
+        <Button onClick={refill}>Refill Tank</Button></div>
       </div>
-
+     
     </div>
   );
 }
