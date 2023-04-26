5cf34d6e3ab9174b52907382dfc7d9909789dd72
ptmertka <71412047+ptmertka@users.noreply.github.com>
2023-04-25 20:39:11 -0500

    Updated PWM

--- arduinoPWM.c
+++ arduinoPWM.c
@@ -1,9 +1,14 @@
+#define bit0 2
+#define bit1 3
+
 unsigned long PWM = 0;
 
 // init serial and pin
 void setup() {
   Serial.begin(9600);
   pinMode(10, INPUT);
+  pinMode(bit0, OUTPUT);
+  pinMode(bit1, OUTPUT);
 }
 
 
@@ -18,6 +23,23 @@ void setup() {
 void loop() {
     //read in the PWM signal on 10, 0.025 second timeout ( I think )
   PWM = pulseIn(10, HIGH, 250000);
+  if(PWM >26000){
+    digitalWrite(bit1, HIGH); // LED ON
+    digitalWrite(bit0, HIGH);
+  }
+  else if(PWM >24000){
+    digitalWrite(bit1, HIGH);
+    digitalWrite(bit0, LOW); // LED ON
+  }
+  else if(PWM > 22000){
+    digitalWrite(bit1, LOW);
+    digitalWrite(bit0, HIGH); // LED ON
+  }
+  else{
+    digitalWrite(bit1, LOW);
+    digitalWrite(bit0, LOW);
+  }
+
   Serial.print(PWM);
   Serial.print('\n');
   delay(5);
